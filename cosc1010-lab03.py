d9fc0bc89d45bf805dd8acd996867bc246f8baf1
JAXSON-V <jvierge2@uwyo.edu>
2024-09-26 22:00:28 +0000

    completed assigment

--- cosc1010-lab03.py
+++ cosc1010-lab03.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Jaxson Viergets
 # UWYO COSC 1010
-# Submission Date
+# Submission Date 9/26/24
 # Lab 03 
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 12
+# Sources, people worked with, help given to: none 
 # your
 # comments
 # here
@@ -17,44 +17,50 @@
 print("Part One------------------------------------------------------------------------")
 #We are going to start with the basics. Declare a list  states that contains the elements: Wyoming, Colorado, Montana in that order 
 #Note this is the ONLY point where you need to declare the states list
-
-
+states = ["Wyoming","Colorado","Montana"]
 
 #print the entire list
-
+print(states[0])
+print(states[1])
+print(states[2])
 
 #now print the first element in the list
-
+print(states[0])
 
 #Print the last element using the syntax shown in class to access the final element (hint, think negatives)
-
+print(states[-1])
 
 #Using an F-string to access the first and second element print the string "COLORADO is south of WYOMING", matching the casing provided
-
+print(f"{states[1]} is south of {states[0]}")
 
 
 
 print("Part Two------------------------------------------------------------------------")
 #Append the following states to your list: Washington, Oregon, California and print your list
-
-
+states.append("Washington")
+states.append("Oregon")
+states.append("California")
+print(states)
 #Again using the specific syntax mentioned in class overwrite the second to last element to be Maine, printing the list 
-
+states[-2]="Maine"
 
 #Insert the state Texas to be the third element in the list, again printing your list
-
-
+states[2]="Texas"
+print(states)
 #Using the `del` statement remove the fourth item from the list, print your list 
-
-
+del states[3]
+print(states)
 #Remove Texas using its value, print the list
-
+del states[2]
 print("Part Three----------------------------------------------------------------------")
 #Temporarily sort your list, print it both sorted and unsorted 
-
+print(sorted(states))
 
 #Permanently sort your list in reverse order, printing it out
-
-
+states.reverse
+print(states)
+states.sort(reverse=True)
+print(states)
 #Using the reverse method reverse the list and print it
-
+states.sort(reverse=True)
+print(states)
